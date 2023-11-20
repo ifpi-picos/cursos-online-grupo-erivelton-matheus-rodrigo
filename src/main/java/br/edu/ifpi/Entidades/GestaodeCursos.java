c5703db900f02c10226d22b41b8089e2b29138a3
Rodrigoaaalves <rodrigoalves2003980@gmail.com>
2023-11-20 15:04:40 -0300

    Implementação da classe Gestão de Cursos

--- src/main/java/br/edu/ifpi/Entidades/GestaodeCursos.java
+++ src/main/java/br/edu/ifpi/Entidades/GestaodeCursos.java
@@ -1,5 +1,35 @@
 package br.edu.ifpi.Entidades;
 
-public class GestaodeCursos {
+import java.util.ArrayList;
+import java.util.List;
 
+public class GestaodeCursos {
+    private List<Curso> cursos;
+
+    public GestaodeCursos(){
+        this.cursos = new ArrayList<>();
+    }
+
+    public void cadastrarCurso(String nome, String status, int cargaHoraria){
+        Curso curso = new Curso(nome, status, cargaHoraria);
+        cursos.add(curso);
+    }
+
+    public void atualizarCurso(String nome, String novoStatus){
+            for(Curso curso : cursos){
+                if(curso.getNome().equals(nome)){
+                    curso.setStatus(novoStatus);
+                    break;
+                }
+            }
+        }
+    
+    public void visualizarCursos(){
+        for(Curso curso : cursos){
+            System.out.println("Nome: "+curso.getNome());
+            System.out.println("Status: "+ curso.getStatus());
+            System.out.println("Carga Horária: "+ curso.getCargaHoraria());
+            System.out.println("*****************************");
+        }
+    }
 }
